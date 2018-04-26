d338b292f907b4dab5230cd2a6cb32c6ca5fbeb3
im-chang <mchang0718@gmail.com>
2018-04-26 11:55:00 -0700

    added fonts

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,26 @@ var gulp = require('gulp'),
 uglify = require("gulp-uglify"),
 rename = require("gulp-rename"),
 browserSync = require("browser-sync"),
-eslint = require("gulp-eslint");
+eslint = require("gulp-eslint"),
+sass = require("gulp-sass"),
+autoprefixer = require("gulp-autoprefixer"),
+cssnano = require("gulp-cssnano"),
+rename = require("gulp-rename");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/style.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./build/css"));
+});
 
 gulp.task('lint', function() {
     return gulp.src(['**/*.js','!node_modules/**'])
@@ -21,6 +40,7 @@ gulp.task("scripts", gulp.series("lint", function() {
 
 gulp.task('watch', function(done) {
     gulp.watch('js/*.js', gulp.series('scripts'));
+    gulp.watch('sass/*.scss', gulp.series('sass'));
     done();
 });
 
@@ -31,8 +51,8 @@ gulp.task("browser-sync", function() {
         }
     });
     
-    gulp.watch(["build/js/*.js", "style.css"])
-    .on('change', browserSync.reload);
+    gulp.watch(["build/js/*.js", "build/css/*.css"])
+    .on("change", browserSync.reload);
 });
 
 gulp.task("default", gulp.parallel("browser-sync", "watch"));
